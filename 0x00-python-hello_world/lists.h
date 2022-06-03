10a7272d2767eb493aa80f3c86c8e12306901643
Daisy-Beverly <aokoowiro@gmail.com>
2022-06-03 06:42:03 +0000

    Fix

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,18 +1,23 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+
 /**
- * check_cycle - function
- * @list: list to check
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: function to check list if there is a cycle
- * Return: 0 if no cycle, 1 if cycle
- */
-int check_cycle(listint_t *list)
+ * Description: singly linked list node structure
+ * for Holberton project
+ **/
+typedef struct listint_s
 {
-  if (list == 0)
-    return (0);
-  else
-    return (1);
-}
+int n;
+struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+#endif /*LISTS_H*/
