0768722f28c4cbac80f56f66e729984a2460726f
Daisy-Beverly <aokoowiro@gmail.com>
2022-06-06 02:27:09 +0000

    Data Structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-len_sentence = len(sentence)
-if (len_sentence == 0):
-new_tuple = (len_sentence, None)
-else:
-new_tuple = (len_sentence, sentence[0])
-return new_tuple
+    len_sen = len(sentence)
+
+    if (len_sen == 0):
+        new_tuple = (len_sen, None)
+    else:
+        new_tuple = (len_sen, sentence[0])
+
+    return (new_tuple)
